1a0c3653bab3b4be234dfa83a39a55d171b4226f
Nguyen <nguyenkhanh41145@gmail.com>
2026-09-14 09:36:34 +0700

    Initial commit for React app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World project using React.js</h1>
+      <p>Dự án đầu tiên thiết lập thành công!</p>
+    </div>
+  )
+}
+
+export default App
