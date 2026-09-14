e5a27540de81fd01d39eb08667a265a3c6c7f7ee
Nguyen <nguyenkhanh41145@gmail.com>
2026-09-14 23:05:14 +0700

    Connect the Render server URL to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
 function App() {
+  const [message, setMessage] = useState('Server connecting...')
+
+  useEffect(() => {
+    // URL RENDER
+    const serverUrl = 'https://server-api-1-4i16.onrender.com/'; 
+
+    fetch(serverUrl)
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => {
+        console.error("ERROR:", err);
+        setMessage('Can not connect to the server. Please check the URL.');
+      });
+  }, [])
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello World project using React.js</h1>
-      <p>Dự án đầu tiên thiết lập thành công!</p>
+    <div className="App" style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Connect ReactJS with Express successfully!</h1>
+      <p>Data from Server: <strong>{message}</strong></p>
     </div>
   )
 }
